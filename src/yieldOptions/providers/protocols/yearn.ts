76071f927869280072d6939680e0a19fe893dfa7
RedVeil <leon.niesler@gmail.com>
2023-09-20 13:03:07 +0200

    fixed yearns api endpoint

--- src/yieldOptions/providers/protocols/yearn.ts
+++ src/yieldOptions/providers/protocols/yearn.ts
@@ -28,7 +28,7 @@ export class Yearn implements IProtocol {
     async getApy(chainId: number, asset: Address): Promise<Yield> {
         let vaults = this.cache.get("vaults") as Vault[];
         if (!vaults) {
-            vaults = (await axios.get(`https://api.yearn.fi/v1/chains/${chainId}/vaults/all`)).data;
+            vaults = (await axios.get(`https://api.yexporter.io/v1/chains/${chainId}/vaults/all`)).data;
             this.cache.set("vaults", vaults);
         }
         const vault = vaults.find((vault: any) => vault.token.address.toLowerCase() === asset.toLowerCase());
