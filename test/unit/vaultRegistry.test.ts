4c9628cd94b070eec12e2e09a274ddc7552654d1
RedVeil <leon.niesler@gmail.com>
2023-10-25 15:23:06 +0200

    vaultRegistry uses objects for inputs

--- test/unit/vaultRegistry.test.ts
+++ test/unit/vaultRegistry.test.ts
@@ -3,7 +3,7 @@ import { publicClient } from "../setup";
 import { VaultRegistry } from "../../src/vaultRegistry";
 
 
-let registry = new VaultRegistry("0x007318Dc89B314b47609C684260CfbfbcD412864", publicClient);
+let registry = new VaultRegistry({ address: "0x007318Dc89B314b47609C684260CfbfbcD412864", publicClient });
 const FORK_BLOCK_NUMBER = BigInt(17883751);
 
 describe.concurrent("read-only", () => {
@@ -23,8 +23,8 @@ describe.concurrent("read-only", () => {
     });
 
     test("getVault() should give back all vaultData for vault at address", async () => {
-        const vaultAddress = "0x5d344226578DC100b2001DA251A4b154df58194f";
-        const vaultData = await registry.getVault(vaultAddress);
+        const vault = "0x5d344226578DC100b2001DA251A4b154df58194f";
+        const vaultData = await registry.getVault({ vault });
 
         expect(vaultData.vault).toBe("0x5d344226578DC100b2001DA251A4b154df58194f");
         expect(vaultData.staking).toBe("0x0000000000000000000000000000000000000000");
@@ -35,7 +35,7 @@ describe.concurrent("read-only", () => {
 
     test("getVaultByDeployer() should give back all vaults deployed by specified creator", async () => {
         const creator = "0x22f5413C075Ccd56D575A54763831C4c27A37Bdb";
-        const vaults = await registry.getVaultByDeployer(creator);
+        const vaults = await registry.getVaultByDeployer({ creator });
 
         expect(vaults[0]).toBe("0x5d344226578DC100b2001DA251A4b154df58194f");
         expect(vaults[1]).toBe("0xc1D4a319dD7C44e332Bd54c724433C6067FeDd0D");
