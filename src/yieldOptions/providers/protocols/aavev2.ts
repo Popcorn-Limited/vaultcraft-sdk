5cf6ee94e3b656da45a788b3332d15a8068b683f
RedVeil <leon.niesler@gmail.com>
2023-11-29 10:43:24 +0100

    fixed aaveV2, compoundV2 dead assets

--- src/yieldOptions/providers/protocols/aavev2.ts
+++ src/yieldOptions/providers/protocols/aavev2.ts
@@ -2,8 +2,10 @@ import { Address, getAddress } from "viem";
 import { ChainToAddress, ProtocolName, Yield } from "src/yieldOptions/types.js";
 import { Clients, IProtocol } from "./index.js";
 import { LENDING_POOL_ABI } from "./abi/aave_v2_lending_pool.js";
+import { DATA_PROVIDER_ABI } from "./abi/aave_v2_data_provider.js";
 
 const LENDING_POOL: ChainToAddress = { 1: "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9" };
+const DATA_PROVIDER: ChainToAddress = { 1: "0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d" }
 
 export class AaveV2 implements IProtocol {
     private clients: Clients;
@@ -46,7 +48,18 @@ export class AaveV2 implements IProtocol {
                 abi: LENDING_POOL_ABI,
                 functionName: "getReservesList",
             }) as Address[]; // viem returns `readonly` type which we don't want
-            return assets.map(asset => getAddress(asset));
+            const configs = await client.multicall({
+                contracts: assets.map(address => {
+                    return {
+                        address: DATA_PROVIDER[chainId],
+                        abi: DATA_PROVIDER_ABI,
+                        functionName: "getReserveConfigurationData",
+                        args: [address]
+                    }
+                }),
+                allowFailure: false
+            })
+            return assets.filter((asset, i) => !configs[i][9]).map((asset, i) => getAddress(asset));
         } catch (e) {
             console.error(e);
             return [];
